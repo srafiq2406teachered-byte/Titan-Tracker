2c16463abcbf87801faba0e3d6c5254a58350a1b
srafiq2406teachered-byte <srafiq2406.teacher.ed@gmail.com>
2026-03-25 14:30:00 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,23 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
+const CACHE_NAME = 'titan-v71-cache';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/static/js/main.chunk.js',
+  '/static/js/0.chunk.js',
+  '/static/js/bundle.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-  // Logic for offline caching can go here later
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
